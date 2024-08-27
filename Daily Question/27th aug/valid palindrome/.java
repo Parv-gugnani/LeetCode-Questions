8b6f9c319cc677a08f0947a98014b11dd0519276
Parv-gugnani <9765.parv@gmail.com>
2024-08-27 17:56:50 +0530

    day 1 completed

--- Daily Question/27th aug/valid palindrome/.java
+++ Daily Question/27th aug/valid palindrome/.java
@@ -1,23 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
-        int right = s.length() -1;
+        int right = s.length() - 1;
 
-        while(left < right){
+        while (left < right) {
             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
+   
             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right++;
+                right--;
             }
 
             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
-            
-        left++;
-        right++;
+
+            left++;
+            right--;
         }
+
         return true;
     }
 }
