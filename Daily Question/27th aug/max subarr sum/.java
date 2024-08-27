90c06384ec41e843c9d12ac3a9581d1435143f9f
Parv-gugnani <9765.parv@gmail.com>
2024-08-27 17:54:22 +0530

    day 1 completed

---
+++ Daily Question/27th aug/max subarr sum/.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int maxsum = nums[0];
+        int currsum = 0;
+
+        for(int num: nums){
+            currsum += num;
+
+        if(currsum > maxsum){
+            maxsum = currsum;
+        }
+
+        if(currsum < 0){
+            currsum =0;
+        }
+        }
+        return maxsum;
+
+    }
+}
